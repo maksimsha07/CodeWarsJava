e88d52acbda6924c75969ce8dcec99ee30ac890c
MaksimCh <cheshikhin.maksim@gmail.com>
2022-03-14 12:49:27 +0300

    Initial commit

---
+++ src/com/company/FirstExample.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class FirstExample {
+    public static int squareSum(int[] n)
+    {
+        int sum = 0;
+        for(int i = 0;i<n.length;i++){
+            sum += n[i]*n[i];
+        }
+        return sum;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] x = new int[] {1,2,2};
+        System.out.println(FirstExample.squareSum(x));
+    }
+}
